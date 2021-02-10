bdefcf5cd1cd2aa87d933cb2b0d5262283ae9a01
Aaron D Borden <adborden@a14n.net>
2021-02-10 15:06:05 -0800

    fixup

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -3,32 +3,10 @@
 set -o errexit
 set -o pipefail
 
-# This script installs some necessary .deb dependencies and then installs binary .whls for the packages
-# specified in vendor-requirements.txt into the "vendor" directory. The actual process runs inside a Docker
-# container; Docker is the only local prerequisite.
-
-# Get the latest version of the cflinuxfs3 image
-docker pull cloudfoundry/cflinuxfs3
-
-# The bind mount here enables us to write back to the host filesystem
-docker run --mount type=bind,source="$(pwd)",target=/home/vcap/app --tmpfs /home/vcap/app/src --name cf_bash --rm -i cloudfoundry/cflinuxfs3  /bin/bash -eu <<EOF
-
-# Go where the app files are
-cd ~vcap/app
-
 # Install any packaged dependencies for our vendored packages
 apt-get -y update
 apt-get -y install swig build-essential python-dev libssl-dev
 
-echo "Past apt-get, downloading pip..."
-
-# Install PIP
-curl -sSL https://bootstrap.pypa.io/2.7/get-pip.py -o /tmp/get-pip.py
-python /tmp/get-pip.py
-
-# As the VCAP user, cache .whls based on the frozen requirements for vendoring
- mkdir -p src vendor
-chown vcap.vcap vendor
-chown vcap.vcap src
-su - vcap -c 'cd app && pip download -r requirements-freeze.txt --no-binary=:none: -d vendor --exists-action=w'
-EOF
+pip install --upgrade --user wheel distutils setuptools
+pip wheel -r requirements-freeze.txt -w vendor --no-binary=:none:
+#pip download -r requirements-freeze.txt --no-binary=:none: -d vendor --exists-action=w
