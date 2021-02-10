1d6fc739248b5155299f641d49829b0520686f0e
Aaron D Borden <adborden@a14n.net>
2021-02-10 15:26:26 -0800

    fixup

--- vendor-requirements.sh
+++ vendor-requirements.sh
@@ -8,5 +8,5 @@ sudo apt-get -y update
 sudo apt-get -y install swig build-essential python-dev libssl-dev
 
 pip install --upgrade --user pip wheel setuptools
+pip download -r requirements-freeze.txt --no-binary=:none: -d vendor --exists-action=w
 pip wheel -r requirements-freeze.txt -w vendor --no-binary=:none:
-#pip download -r requirements-freeze.txt --no-binary=:none: -d vendor --exists-action=w
